697c5ef92c41efc3513eb6517a060bdec0790046
juliflorezg <forezjuli08@hotmail.com>
2022-12-21 16:53:15 -0500

    add navigation to FlatListItem

--- src/components/FlatListMenuItem.tsx
+++ src/components/FlatListMenuItem.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {MenuItem} from '../interfaces/menuItem';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import {useNavigation} from '@react-navigation/native';
+import {RootStackParams} from '../navigation/Navigator';
+import {StackNavigationProp} from '@react-navigation/stack';
 
 interface Props {
   menuItem: MenuItem;
 }
 
+type NavigationProp = StackNavigationProp<RootStackParams>;
+
 export const FlatListMenuItem = ({menuItem}: Props) => {
+  const {navigate} = useNavigation<NavigationProp>();
+
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={() => navigate(menuItem.component as keyof RootStackParams)}>
       <View style={styles.container}>
         <Icon name={menuItem.icon} color="grey" size={20} />
         <Text style={styles.itemText}>
