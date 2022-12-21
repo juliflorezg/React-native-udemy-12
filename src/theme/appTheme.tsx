ffce15265f5d26135c9bed3fceb88b669bb463d5
juliflorezg <forezjuli08@hotmail.com>
2022-12-21 14:51:53 -0500

    add global styles

---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: 'crimson',
+  },
+  globalMargin: {
+    marginHorizontal: 20,
+  },
+});
