907eb1354050c76548522171e3d6296b91a5af0e
juliflorezg <forezjuli08@hotmail.com>
2022-12-21 09:02:48 -0500

    create the navigation

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
 export const HomeScreen = () => {
   return (
     <View>
       <Text>Home Screen</Text>
+      <Text>
+        <Icon name="water" size={40} color="blue" />
+      </Text>
     </View>
   );
 };
