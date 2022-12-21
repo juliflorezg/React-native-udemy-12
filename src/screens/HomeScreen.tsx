d9812ca37db48f726f049e3bcfc66734aa36c23c
juliflorezg <forezjuli08@hotmail.com>
2022-12-21 10:20:22 -0500

    add FlatList to home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
+interface MenuItem {
+  name: string;
+  icon: string;
+  components: string;
+}
+
+const menuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'cube',
+    components: 'Animation101Screen',
+  },
+];
+
 export const HomeScreen = () => {
+  const renderMenuItem = (menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text>
+          {menuItem.name} - {menuItem.icon}
+        </Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>Home Screen</Text>
-      <Text>
-        <Icon name="water" size={40} color="blue" />
-      </Text>
+    <View style={{flex: 1, backgroundColor: 'lightgrey'}}>
+      <FlatList
+        data={menuItems}
+        renderItem={({item}) => renderMenuItem(item)}
+        keyExtractor={item => item.name}
+      />
     </View>
   );
 };
