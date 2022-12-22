ea1a79ec3a7454167f498a23f10aa105466fe47e
juliflorezg <forezjuli08@hotmail.com>
2022-12-22 09:38:01 -0500

    add animation for bouncing

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,15 +1,23 @@
 import React, {useRef} from 'react';
-import {Animated, Button, StyleSheet, View} from 'react-native';
+import {Animated, Button, Easing, StyleSheet, View} from 'react-native';
 
 export const Animation101Screen = () => {
   const opacity = useRef(new Animated.Value(0)).current;
+  const top = useRef(new Animated.Value(-100)).current;
 
   const fadeIn = () => {
     Animated.timing(opacity, {
       toValue: 1,
-      duration: 300,
+      duration: 400,
       useNativeDriver: true,
     }).start(() => console.log('fade in animation finished!'));
+
+    Animated.timing(top, {
+      toValue: 0,
+      duration: 800,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    }).start();
   };
   const fadeOut = () => {
     Animated.timing(opacity, {
@@ -17,11 +25,29 @@ export const Animation101Screen = () => {
       duration: 300,
       useNativeDriver: true,
     }).start();
+    Animated.timing(top, {
+      toValue: -100,
+      duration: 800,
+      useNativeDriver: true,
+    }).start();
   };
 
   return (
     <View style={styles.container}>
-      <Animated.View style={{...styles.purpleBox, opacity, marginBottom: 20}} />
+      <Animated.View
+        //! Style property 'top' is not supported by native animated module (we can't animate top property)
+        // style={{...styles.purpleBox, opacity, marginBottom: 20, top}}
+        style={{
+          ...styles.purpleBox,
+          opacity,
+          marginBottom: 20,
+          transform: [
+            {
+              translateY: top,
+            },
+          ],
+        }}
+      />
       <Button title="fade in" onPress={fadeIn} />
       <Button title="fade out" onPress={fadeOut} />
     </View>
