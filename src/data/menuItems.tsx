fbb458fbb762fba0158f62f854428c002f4fc114
juliflorezg <forezjuli08@hotmail.com>
2022-12-22 16:45:23 -0500

    add items data file

---
+++ src/data/menuItems.tsx
@@ -0,0 +1,19 @@
+import {MenuItem} from '../interfaces/menuItem';
+
+export const menuItems: MenuItem[] = [
+  {
+    name: 'Animation 101',
+    icon: 'cube',
+    component: 'Animation101Screen',
+  },
+  {
+    name: 'Animation 102',
+    icon: 'shield-alt',
+    component: 'Animation102Screen',
+  },
+  {
+    name: 'Switch Component',
+    icon: 'toggle-on',
+    component: 'SwitchScreen',
+  },
+];
