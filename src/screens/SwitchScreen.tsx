9fe2e7c834bd0260cbe5fe8e8bc7bbe5fc64a6cd
juliflorezg <forezjuli08@hotmail.com>
2022-12-23 08:32:28 -0500

    add switch rows

--- src/screens/SwitchScreen.tsx
+++ src/screens/SwitchScreen.tsx
@@ -9,7 +9,16 @@ export const SwitchScreen = () => {
     isHappy: true,
   });
 
-  const onChange = (value: boolean, field: keyof typeof state) => {
+  //* this extracts the types for every key in 'state'
+  // type StateType = {
+  //   isActive: boolean;
+  //   isHungry: boolean;
+  //   isHappy: boolean;
+  // };
+  type StateType = typeof state;
+  type Field = keyof StateType;
+
+  const onChange = (field: Field, value: boolean) => {
     setState({
       ...state,
       [field]: value,
@@ -26,7 +35,21 @@ export const SwitchScreen = () => {
         <Text style={styles.switchText}>isActive</Text>
         <CustomSwitch
           isOn={state.isActive}
-          onChange={value => onChange(value, 'isActive')}
+          onChange={value => onChange('isActive', value)}
+        />
+      </View>
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isHungry</Text>
+        <CustomSwitch
+          isOn={state.isHungry}
+          onChange={value => onChange('isHungry', value)}
+        />
+      </View>
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isHappy</Text>
+        <CustomSwitch
+          isOn={state.isHappy}
+          onChange={value => onChange('isHappy', value)}
         />
       </View>
 
@@ -40,6 +63,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    marginBottom: 5,
   },
   switchText: {
     fontSize: 25,
