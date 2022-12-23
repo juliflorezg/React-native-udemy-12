c6b5f81cb555feb9a3d58bc7fdce423bbc8a56d8
juliflorezg <forezjuli08@hotmail.com>
2022-12-23 10:02:19 -0500

    add AlertScreen to navigator

---
+++ src/screens/AlertScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+import {HeaderTitle} from '../components/HeaderTitle';
+export const AlertScreen = () => {
+  return (
+    <View>
+      <HeaderTitle title="Alert component" />
+    </View>
+  );
+};
