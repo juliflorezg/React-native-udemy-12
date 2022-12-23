df5ca28f9437f6ae07b9aaee6e2089bab241e162
juliflorezg <forezjuli08@hotmail.com>
2022-12-23 10:24:51 -0500

    add alert to alertscreen

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Alert, Button, View} from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
+import {styles} from '../theme/appTheme';
 export const AlertScreen = () => {
+  const showAlert = () => {
+    Alert.alert(
+      'My First Alert',
+      'Generic alert message',
+      [
+        {text: 'CanCel', onPress: () => console.log('cancel pressed~!!')},
+        {text: 'ok', onPress: () => console.log('OK pressed~!!')},
+      ],
+      {
+        cancelable: true,
+        onDismiss: () => console.log('alert dismissed =('),
+      },
+    );
+  };
+
   return (
-    <View>
+    <View style={styles.globalMargin}>
       <HeaderTitle title="Alert component" />
+      <Button title="Show Alert" onPress={showAlert} />
     </View>
   );
 };
