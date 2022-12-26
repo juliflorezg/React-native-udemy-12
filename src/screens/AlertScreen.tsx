d3af4bd62b8b8c39395658b47ce689ccf23f5dd8
juliflorezg <forezjuli08@hotmail.com>
2022-12-26 09:20:06 -0500

    add prompt android to AlertScreen

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Alert, Button, View} from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
+import prompt from 'react-native-prompt-android';
+
 export const AlertScreen = () => {
   const showAlert = () => {
     Alert.alert(
@@ -18,10 +20,36 @@ export const AlertScreen = () => {
     );
   };
 
+  const showPrompt = () => {
+    prompt(
+      'Enter password',
+      'Enter your password to claim your $1.5B in lottery winnings',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: password => console.log('OK Pressed, password: ' + password),
+        },
+      ],
+      {
+        type: 'secure-text',
+        cancelable: false,
+        defaultValue: 'test',
+        placeholder: 'placeholder',
+      },
+    );
+  };
+
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title="Alert component" />
       <Button title="Show Alert" onPress={showAlert} />
+      <View style={{height: 10}} />
+      <Button title="Show Prompt" onPress={showPrompt} />
     </View>
   );
 };
