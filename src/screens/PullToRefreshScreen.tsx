36828290ecd84a118271363b8eafa15bdb83ed5d
juliflorezg <forezjuli08@hotmail.com>
2022-12-26 15:11:08 -0500

    add styles to pull to refresh screen

--- src/screens/PullToRefreshScreen.tsx
+++ src/screens/PullToRefreshScreen.tsx
@@ -2,8 +2,11 @@ import React, {useState} from 'react';
 import {RefreshControl, ScrollView, View} from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 export const PullToRefreshScreen = () => {
+  const {top} = useSafeAreaInsets();
   const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState<string>();
 
   const onRefresh = () => {
     setRefreshing(true);
@@ -12,16 +15,27 @@ export const PullToRefreshScreen = () => {
       console.log("We're done");
 
       setRefreshing(false);
-    }, 1500);
+      setData('Hello world!');
+    }, 3500);
   };
 
   return (
     <ScrollView
+      style={{
+        marginTop: refreshing ? top : 0,
+      }}
       refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          progressViewOffset={10}
+          progressBackgroundColor="lightgrey"
+          colors={['white', 'turquoise', 'purple']}
+        />
       }>
       <View style={styles.globalMargin}>
         <HeaderTitle title="Pull To Refresh" />
+        {data && <HeaderTitle title={data} />}
       </View>
     </ScrollView>
   );
