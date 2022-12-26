34254cac25595b7f29248c309a4160ea2c655eba
juliflorezg <forezjuli08@hotmail.com>
2022-12-26 18:58:06 -0500

    add loadMore function

--- src/screens/InfiniteScrollScreen.tsx
+++ src/screens/InfiniteScrollScreen.tsx
@@ -4,16 +4,28 @@ import {HeaderTitle} from '../components/HeaderTitle';
 // import { styles } from '../theme/appTheme';
 export const InfiniteScrollScreen = () => {
   const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
+
+  const loadMore = () => {
+    const newArray: number[] = [];
+
+    for (let i = 0; i < 5; i++) {
+      newArray[i] = numbers.length + i;
+    }
+
+    setNumbers([...numbers, ...newArray]);
+  };
   const renderItem = (item: number) => {
     return <Text style={styles.textItem}>{item}</Text>;
   };
   return (
-    <View>
+    <View style={{flex: 1, backgroundColor: 'turquoise'}}>
       <FlatList
         data={numbers}
         renderItem={({item}) => renderItem(item)}
         keyExtractor={item => String(item)}
         ListHeaderComponent={() => <HeaderTitle title="Infinite Scroll" />}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.5}
       />
     </View>
   );
