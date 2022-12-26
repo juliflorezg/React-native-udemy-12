13a50a126c3cc2fc53c1f0c4415c0924765462de
juliflorezg <forezjuli08@hotmail.com>
2022-12-26 10:59:14 -0500

    add KeyBoardAvoidingView

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,5 +1,15 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
 export const TextInputScreen = () => {
@@ -23,44 +33,60 @@ export const TextInputScreen = () => {
   console.log(setForm);
 
   return (
-    <View style={styles.globalMargin}>
-      <HeaderTitle title="TextInput" />
-      <View>
-        <Text>Name</Text>
-      </View>
-      <TextInput
-        style={textStyle.input}
-        placeholder="Your name"
-        autoCorrect={false}
-        autoCapitalize="words"
-        onChangeText={value => onChange('name', value)}
-      />
-      <View>
-        <Text>Email</Text>
-      </View>
-      <TextInput
-        style={textStyle.input}
-        placeholder="Your email"
-        autoCorrect={false}
-        autoCapitalize="none"
-        keyboardType="email-address"
-        onChangeText={value => onChange('email', value)}
-      />
-      <View>
-        <Text>Phone number</Text>
-      </View>
-      <TextInput
-        style={textStyle.input}
-        placeholder="Your phone"
-        keyboardType="phone-pad"
-        onChangeText={value => onChange('phone', value)}
-      />
-      <HeaderTitle title={JSON.stringify(form, null, 2)} />
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'android' ? 'height' : 'padding'}
+      style={textStyle.container}>
+      <ScrollView>
+        {
+          //* This touchable without feedback component allows us to close the keyboard whenever we press on the screen
+        }
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.globalMargin}>
+            <HeaderTitle title="TextInput" />
+            <View>
+              <Text>Name</Text>
+            </View>
+            <TextInput
+              style={textStyle.input}
+              placeholder="Your name"
+              autoCorrect={false}
+              autoCapitalize="words"
+              onChangeText={value => onChange('name', value)}
+            />
+            <View>
+              <Text>Email</Text>
+            </View>
+            <TextInput
+              style={textStyle.input}
+              placeholder="Your email"
+              autoCorrect={false}
+              autoCapitalize="none"
+              keyboardType="email-address"
+              onChangeText={value => onChange('email', value)}
+            />
+            <View>
+              <Text>Phone number</Text>
+            </View>
+            <HeaderTitle title={JSON.stringify(form, null, 2)} />
+            <HeaderTitle title={JSON.stringify(form, null, 2)} />
+            <TextInput
+              style={textStyle.input}
+              placeholder="Your phone"
+              keyboardType="phone-pad"
+              onChangeText={value => onChange('phone', value)}
+            />
+            <View style={{height: 100}} />
+          </View>
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
 const textStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   input: {
     borderWidth: 2,
     height: 50,
