32abc398fdb0f00f7faa4bff630a3b1983517c83
juliflorezg <forezjuli08@hotmail.com>
2022-12-28 16:46:15 -0500

    take NavigationContainer to Navigator component this must be done in order to theme to work correctly, otherwise if we let if in App.tsx it won't recognize the colors

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 // import {View, Text} from 'react-native';
-import {
-  // DarkTheme,
-  // DefaultTheme,
-  NavigationContainer,
-  // Theme,
-} from '@react-navigation/native';
+import // DarkTheme,
+// DefaultTheme,
+// NavigationContainer,
+// Theme,
+'@react-navigation/native';
 import {Navigator} from './src/navigation/Navigator';
 import {ThemeProvider} from './src/context/theme/ThemeContext';
 // import {useColorScheme} from 'react-native';
@@ -21,13 +20,14 @@ import {ThemeProvider} from './src/context/theme/ThemeContext';
 
 const App = () => {
   // const scheme = useColorScheme();
+
   return (
     <ThemeProvider>
-      <NavigationContainer
-      // theme={scheme === 'dark' ? customTheme : DefaultTheme}
-      >
-        <Navigator />
-      </NavigationContainer>
+      {/*theme={scheme === 'dark' ? customTheme : DefaultTheme}
+        theme={theme} */}
+
+      <Navigator />
+      {/* </NavigationContainer> */}
     </ThemeProvider>
   );
 };
