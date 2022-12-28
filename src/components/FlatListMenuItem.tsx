73e2e63b885f2c5a180260f4a9153e6ca27e0136
juliflorezg <forezjuli08@hotmail.com>
2022-12-28 16:48:53 -0500

    use context in menu item to get colors

--- src/components/FlatListMenuItem.tsx
+++ src/components/FlatListMenuItem.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import {MenuItem} from '../interfaces/menuItem';
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import {useNavigation, useTheme} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import {RootStackParams} from '../navigation/Navigator';
 import {StackNavigationProp} from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import {MenuItem} from '../interfaces/menuItem';
+import {ThemeContext} from '../context/theme/ThemeContext';
 
 interface Props {
   menuItem: MenuItem;
@@ -15,20 +16,25 @@ type NavigationProp = StackNavigationProp<RootStackParams>;
 export const FlatListMenuItem = ({menuItem}: Props) => {
   const {navigate} = useNavigation<NavigationProp>();
 
-  const {colors} = useTheme();
+  // const {colors} = useTheme();
+
+  const {
+    theme: {colors},
+  } = useContext(ThemeContext);
+
   return (
     <TouchableOpacity
       activeOpacity={0.8}
       onPress={() => navigate(menuItem.component as keyof RootStackParams)}>
       <View style={styles.container}>
-        <Icon name={menuItem.icon} color="#8f00cd" size={20} />
+        <Icon name={menuItem.icon} color={colors.primary} size={20} />
         <Text style={{...styles.itemText, color: colors.text}}>
           {menuItem.name}
         </Text>
         <Icon
           name="caret-right"
           size={20}
-          color="#8f00cd"
+          color={colors.primary}
           style={styles.goIcon}
         />
       </View>
